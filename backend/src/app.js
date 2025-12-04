689ccb4ed9de56e18d71bbbbc2deee254840effe
Ritik Sahu <sahuritik431@gmail.com>
2025-12-04 21:21:00 +0530

    Fix typo in food-partner.routes import

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/auth.routes');
-const foodPartnerRoutes = require('./routes/food-parter.routes');
+const foodPartnerRoutes = require('./routes/food-partner.routes');
 const foodRoutes = require('./routes/food.routes');
 
 const app = express();
